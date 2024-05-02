372b4585d2b83c3c88cf3ad247b3555e54a26816
PRAGYAN LAMBA <115528212+PragyanLamba@users.noreply.github.com>
2024-05-02 11:48:34 +0000

    tcs

---
+++ tcs prepr/primeisperfect.py
@@ -0,0 +1,26 @@
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range (2,int(num**0.5)+1):
+        if num %i==0:
+            return False
+    return True
+def isperfect (num):
+    if num <0:
+        return False
+    sqr= int(num ** 0.5)
+    return sqr*sqr==num
+n=int(input("enter the number"))
+arr=[]
+sqr2=1
+sqr3=1
+for i in range(0,n):
+    if (is_prime(i+1)):
+        arr[i]=sqr2
+        sqr2*=2
+    elif(isperfect(i+1)):
+        arr[i]=sqr3
+        sqr3*=3
+    else:
+        arr[i]=arr[i-1]=arr[i-2] 
+print (arr[n-1])       
